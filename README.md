94d340215d9d790579738061a9dcab898f028371
Lucas Mantovani <lrmantovani10@gmail.com>
2022-06-17 14:58:26 -0700

    Project Upload

--- README.md
+++ README.md
@@ -2,68 +2,72 @@
 
 ## Week 1 Assignment: Flixster
 
-Submitted by: **NAME**
+Submitted by: Lucas Mantovani
 
-Estimated time spent: **#** hours spent in total
+Estimated time spent: 12 hours spent in total
 
-Deployed Application (optional): [Flixster Deployed Site](ADD_LINK_HERE)
+Deployed Application (optional): [Flixster Deployed Site](https://lrmantovani10.github.io/Flixster/)
 
 ### Application Features
 
 #### CORE FEATURES
 
-- [ ] User can view a list of current movies from The Movie Database API as a grid view
+- [X] User can view a list of current movies from The Movie Database API as a grid view
   - The grid element should have an id of `movies-grid`
   - Each movie wrapper element should have a class of `movie-card`
-- [ ] For each movie displayed, user can see the following details:
+- [X] For each movie displayed, user can see the following details:
   - Title - the element should have a class of `movie-title`
   - Image - the `img` element should have a class of `movie-poster`
   - Votes - the element should have a class of `movie-votes`
-- [ ] User can load more current movies by clicking a button at the bottom of the list
+- [X] User can load more current movies by clicking a button at the bottom of the list
   - The button should have an id of `load-more-movies-btn`.
   - When clicked, the page should not refresh.
   - New movies should simply be added to the bottom
-- [ ] Allow users to search for movies and display them in a grid view
+- [X] Allow users to search for movies and display them in a grid view
   - There should be a search input element with an id of `search-input`
   - Users should be able to type into the input
   - When a user hits 'Enter', it should send a search request to the movies API
   - The results from the search should be displayed on the page
   - There should be a close icon with an id of `close-search-btn` that exits the search, clears results, and shows the current movies displayed previously
-- [ ] Website accounts for basic HTML/CSS accessibility features
-- [ ] Website should be responsive
+- [X] Website accounts for basic HTML/CSS accessibility features
+- [X] Website should be responsive
 
 #### STRETCH FEATURES
 
-- [ ] Deploy website using GitHub Pages. 
-- [ ] Allow user to view more details about a movie within a popup.
-- [ ] Improve the user experience through CSS & animation.
-- [ ] Allow movie video trailers to be played using [embedded YouTube](https://support.google.com/youtube/answer/171780?hl=en)
-- [ ] Implement anything else that you can get done to improve the app functionality!
+- [X] Deploy website using GitHub Pages. 
+- [X] Allow user to view more details about a movie within a popup.
+- [X] Improve the user experience through CSS & animation.
+- [X] Allow movie video trailers to be played using [embedded YouTube](https://support.google.com/youtube/answer/171780?hl=en)
+- [X] Implement anything else that you can get done to improve the app functionality!
 
 ### Walkthrough Video
 
-`TODO://` Add the embedded URL code to your animated app walkthrough below, `ADD_EMBEDDED_CODE_HERE`. Make sure the video or gif actually renders and animates when viewing this README. (🚫 Remove this paragraph after adding walkthrough video)
-
-`ADD_EMBEDDED_CODE_HERE`
+Open walkthrough.mov file in repo
 
 ### Reflection
 
 * Did the topics discussed in your labs prepare you to complete the assignment? Be specific, which features in your weekly assignment did you feel unprepared to complete?
 
-Add your response here
+The CSS features covered on the first lab (grid, flexbox), as well as the HTML (through as divs and spans) prepared me for this assignment. I also think that the second lab's JavaScript DOM manipulation and the third one's async functionalities helped me prepare for Flixster. 
 
 * If you had more time, what would you have done differently? Would you have added additional features? Changed the way your project responded to a particular event, etc.
   
-Add your response here
+If I had more time, I would have added filters so that the user could determine what movies should
+show up on the top of the page (for example, filters by rating or release date)
 
 * Reflect on your project demo, what went well? Were there things that maybe didn't go as planned? Did you notice something that your peer did that you would like to try next time?
 
-Add your response here
+I really liked everything I did, but I saw a girl in my group who added a feature which allowed the user to 
+flip the poster when clicking on it, so that might be a cool idea for next time!
 
 ### Open-source libraries used
 
 - Add any links to open-source libraries used in your project.
 
+N/A
+
 ### Shout out
 
 Give a shout out to somebody from your cohort that especially helped you during your project. This can be a fellow peer, instructor, TA, mentor, etc.
+
+Vincent Chen
---
+++ script.js
@@ -0,0 +1,164 @@
+const apiKey = "4b04f34e01e13309a6742e874874ef9"
+const basePath = 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/'
+const videoBasePath = "https://www.youtube.com/embed/"
+let pageNumber = 1
+let searching = false
+let accumulatedResults = []
+let currentText = ''
+
+function turnOffDisplay(){
+  document.getElementById("modalContent").style.display = "none"
+  document.body.style.overflow = "auto"
+  const movieTrailer = document.getElementById("movieTrailer")
+  if(movieTrailer.style.display != "none"){
+    toggleImage()
+  }
+}
+
+function loadMore(){
+  pageNumber += 1
+  fetchMovies()
+}
+
+const getExtraInfo = async(movieId) =>{
+  try{
+    const response = await fetch('https://api.themoviedb.org/3/movie/' + movieId + '?api_key='+apiKey+'8&language=en-US&query='+currentText+'&page='+pageNumber);
+    const loadData = await response.json()
+    return [loadData.genres, loadData.runtime, loadData.homepage] 
+  }
+  catch(error){
+    console.error("Error fetching extra movie data: " + error)
+  }
+}
+
+const getMovieTrailer = async(movieId) =>{
+  try{
+    const response = await fetch('https://api.themoviedb.org/3/movie/' + movieId + '/videos?api_key='+apiKey+'8&language=en-US');
+    const loadData = await response.json()
+    let returnSrc
+    loadData.results.forEach(
+      trailerElement => {
+        if(trailerElement.type == 'Trailer' || trailerElement.type == 'trailer'){
+          returnSrc = trailerElement.key
+        }
+      }
+    )
+    return returnSrc
+  }
+  catch(error){
+    console.error("Error fetching movie trailer: " + error)
+  }
+}
+
+function showModal(movieContent){
+  const modalDisplay = document.getElementById("modalContent")
+  const movieCard = document.getElementById("movie-card")
+  const subTitle = document.getElementById("subTitle")
+  const extraInfo = document.getElementById("extraInfo")
+  const summary = document.getElementById("summary")
+  const exitButton = document.getElementById("close")
+  const movieTrailer = document.getElementById("movieTrailer")
+  document.body.style.overflow = "hidden"
+  modalDisplay.style.display = "grid"
+  movieCard.src = basePath + movieContent.backdrop_path
+  movieCard.alt = movieContent.title + " preview image"
+  movieCard.addEventListener("click", toggleImage)
+  subTitle.innerHTML = movieContent.title
+  summary.innerHTML = movieContent.overview
+  exitButton.addEventListener("click", turnOffDisplay)
+  getExtraInfo(movieContent.id).then((additionalInfo)=>{
+    subTitle.href = additionalInfo[2]
+    let genres = ''
+    for(let i = 0; i < additionalInfo[0].length; i++){
+      if(i != 0)
+        genres += (", " + additionalInfo[0][i].name)
+      else
+        genres += additionalInfo[0][i].name
+    }
+    extraInfo.innerHTML = genres + " | " + additionalInfo[1] + "mins | Release Date: " + movieContent.release_date + " | Rating: " + movieContent.vote_average
+    
+    getMovieTrailer(movieContent.id).then((trailerSrc) =>{
+      movieTrailer.src = videoBasePath + trailerSrc
+      movieTrailer.alt = movieContent.title + " YouTube trailer"
+    })
+ 
+  })
+}
+
+function toggleImage(){
+  const movieCard = document.getElementById("movie-card")
+  const movieTrailer = document.getElementById("movieTrailer")
+  
+  if(movieCard.style.display != "none"){
+    movieCard.style.animation = "fadeOut 0.6s"
+    setTimeout(() => {
+      movieCard.style.display = "none"
+      movieTrailer.style.display = "block"
+    },600)
+  }
+  else{
+    movieCard.style.animation = ""
+    movieCard.style.display = "block"
+    movieTrailer.style.display = "none"
+  }
+}
+
+const fetchMovies = async() =>{
+  try{
+    let response
+    if(searching){
+      response = await fetch('https://api.themoviedb.org/3/search/movie?api_key='+apiKey+'8&language=en-US&query='+currentText+'&page='+pageNumber);
+    }
+    else{
+      response = await fetch('https://api.themoviedb.org/3/movie/now_playing?api_key='+apiKey+'8&language=en-US&page='+pageNumber);
+    }
+    const loadData = await response.json()
+    const myGrid = document.getElementById("movies-grid")
+    if(pageNumber == 1)
+      myGrid.innerHTML = ``
+    for (let i = 0; i < loadData.results.length; i++){
+      myGrid.innerHTML += `
+      <div class = "newMovie">
+        <h2 class = "movie-title"> ${loadData.results[i].title} </h2>
+        <img src = "${basePath + loadData.results[i].poster_path}" class = "movie-poster" alt = "${loadData.results[i].title} poster that opens popup when clicked">
+        <h3 class = "movie-votes">${loadData.results[i].vote_average}</h3>
+      </div>`
+    }
+    const movies = document.querySelectorAll(".movie-poster")
+    accumulatedResults = accumulatedResults.concat(loadData.results)
+    for (let i = 0; i < accumulatedResults.length; i++){
+      movies[i].addEventListener("click", () => showModal(accumulatedResults[i]))
+    }
+    
+  }
+  catch(error){
+    console.error("Error fetching movie data: " + error)
+  }
+}
+
+window.onload = function () {
+  fetchMovies()  
+  const loadButton = document.getElementById("load-more-movies-btn")
+  loadButton.addEventListener("click", loadMore)
+}
+
+function searchResult(event){
+  event.preventDefault();
+  const userText = document.getElementById("searchBar").value
+  if(userText.length){
+    currentText = userText
+    pageNumber = 1
+    searching = true
+    accumulatedResults = []
+    fetchMovies()
+  }
+}
+
+function goToMain(){
+  if(searching){
+    pageNumber = 1
+    accumulatedResults = []
+    searching = false
+    fetchMovies()
+  }
+}
